67b6ac03cca24a2baf1ee5fe1d49becf111ac5c3
Sergi Llull Puente <sergillullpuente@gmail.com>
2022-11-20 14:09:59 +0100

    feat: initial value-objects and configure eslint

---
+++ libs/commons/src/lib/value-object/string.value-object.ts
@@ -0,0 +1,7 @@
+import { ValueObject } from './value-object';
+
+export abstract class StringValueObject extends ValueObject<string> {
+    constructor(value: string) {
+        super(value);
+    }
+}
