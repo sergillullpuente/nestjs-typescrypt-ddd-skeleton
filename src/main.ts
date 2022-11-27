e15f009da8bda578bb15bc203e33612e834a6414
Sergi Llull Puente <sergillullpuente@gmail.com>
2022-11-27 12:16:28 +0100

    feat: microservice users

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from "@nestjs/core";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {Logger} from "@nestjs/common";
+import {UsersModule} from "./users.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(UsersModule);
+    const options = new DocumentBuilder()
+        .setTitle("[User]")
+        .setDescription("MicroService Users")
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup("docs", app, document);
+    await app.listen(3000);
+    Logger.log(
+        "🚀 Application is running on: http://localhost:3000"
+    );
 }
+
 bootstrap();
